25ec843746f7cf62247dd920233ffaa4b92e59bf
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-05 14:38:53 +0530

    steps to switch section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import StepsToSwitch from './components/StepsToSwitch'
 import SwitchingEnergy from './components/SwitchingEnergy'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <SwitchingEnergy />
+      <StepsToSwitch/>
     </>
   )
 }
