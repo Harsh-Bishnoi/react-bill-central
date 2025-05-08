54377652828207cb890a51e10ee23f55a27cc339
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-08 14:57:51 +0530

    solve mistake in testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,44 +1,38 @@
 import React from 'react'
 import CustomHeading from './common/CustomHeading'
 import CustomDescription from './common/CustomDescription'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { SWIPER_DATA_2 } from '../utils/helper'
+import { Autoplay, Navigation } from 'swiper/modules'
+import { SwiperNextArrow, SwiperPrevArrow } from '../utils/icon'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { SWIPER_DATA_2 } from '../utils/helper';
-import { Autoplay, Navigation } from 'swiper/modules';
-import { SwiperNextArrow, SwiperPrevArrow } from '../utils/icon';
 const Testimonials = () => {
     return (
         <>
-            <div className="flex justify-center items-center pb-10 sm:pb-20 md:pb-30 lg:pb-40.5">
-                <div className="max-w-[1284px] px-3">
+            <div className="w-full px-3 sm:px-6 overflow-hidden pb-10 sm:pb-20 md:pb-30 lg:pb-40.5">
+                <div className="max-w-full mx-auto">
                     <CustomHeading headingClass="pb-4 text-center" headingtext="Our" headingSpan2="Testimonials" />
                     <CustomDescription descriptionClass="max-w-[490px] mx-auto text-center pb-15.5" descriptionText="Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum" />
-                    <div className="relative max-w-[1284px]">
-                        <div className="flex items-center justify-between">
-                            <div className="next-btn absolute left-[-68px] z-10 top-[45%] w-10 h-10 rounded-[34px] border border-[#00171F] flex items-center justify-center hover:bg-[#00171F] swiper-arrow transition-all duration-200 ease-linear cursor-pointer"><SwiperPrevArrow /></div>
-                            <div className="prev-btn absolute right-[-68px] z-10 top-[45%] w-10 h-10 rounded-[34px] border border-[#00171F] flex items-center justify-center hover:bg-[#00171F] swiper-arrow transition-all duration-200 ease-linear cursor-pointer"><SwiperNextArrow /></div>
-                        </div>
+                    <div className="relative max-w-[1284px] mx-auto">
                         <div>
-                            <Swiper className='max-w-[1140px]' navigation={{
-                                nextEl: '.next-btn',
-                                prevEl: '.prev-btn',
-                            }}
-                                modules={[Navigation, Autoplay]}
+                            <Swiper
+                                className="max-w-[1140px] overflow-hidden"
+                                navigation={{
+                                    nextEl: '.next-btn,.btn-1',
+                                    prevEl: '.prev-btn',
+                                }}
+                                modules={[Navigation]}
                                 spaceBetween={24}
                                 slidesPerView={3}
                                 loop={true}
-                                autoplay={{
-                                    delay: 1500,
-                                    disableOnInteraction: false,
-                                }}
                                 breakpoints={{
                                     320: {
                                         slidesPerView: 1,
                                     },
                                     640: {
-                                        slidesPerView: 1,
+                                        slidesPerView: 2,
                                     },
                                     768: {
                                         slidesPerView: 2,
@@ -50,21 +44,29 @@ const Testimonials = () => {
                                 onSlideChange={() => console.log('slide change')}
                             >
                                 {SWIPER_DATA_2.map((items, index) => (
-                                    <SwiperSlide className='max-w-[364px] mx-auto'>
-                                        <div id={index} className="max-w-[364px] border-[#0000001A] border rounded-sm bg-white shadow-[0px_0px_13.9px_0px_#0000001A] p-5">
-                                            <div className="flex gap-2 mb-6">
-                                                <img src={items.img} alt="img" />
+                                    <SwiperSlide key={index} className="w-full">
+                                        <div id={index} className="border-[#0000001A] border rounded-sm bg-white shadow-[0px_0px_13.9px_0px_#0000001A] p-5">
+                                            <div className="flex gap-2 mb-6 items-center">
+                                                <img className="" src={items.img} alt="img" />
                                                 <div className="text-start">
-                                                    <p className='font-family-primary font-normal text-base leading-[150%] text-black'>{items.name}</p>
-                                                    <p className='font-family-primary font-normal text-base leading-[150%] text-black opacity-50'>{items.id}</p>
+                                                    <p className="font-family-primary font-normal text-2xl leading-[150%] text-black">{items.name}</p>
+                                                    <p className="font-family-primary font-normal text-base leading-[150%] text-black opacity-50">{items.id}</p>
                                                 </div>
                                             </div>
-                                            <img className='mb-4.5' src={items.star} alt="star" />
-                                            <p className='text-start font-family-primary font-normal text-base leading-[150%] text-[#00171F] opacity-90'>{items.para}</p>
+                                            <img className="mb-4.5" src={items.star} alt="star" />
+                                            <p className="text-start font-family-primary font-normal text-base leading-[150%] text-[#00171F] opacity-90">{items.para}</p>
                                         </div>
                                     </SwiperSlide>
                                 ))}
                             </Swiper>
+                        </div>
+                        <div className="flex items-center max-xl:gap-5 justify-between max-xl:justify-center">
+                            <div className="prev-btn xl:absolute xl:left-0 z-10 top-1/2 max-xl:mt-5 xl:transform xl:-translate-y-1/2 w-10 h-10 rounded-full border border-[#00171F] flex items-center justify-center hover:bg-[#00171F] swiper-arrow transition-all duration-200 ease-linear cursor-pointer">
+                                <SwiperPrevArrow />
+                            </div>
+                            <div className="next-btn xl:absolute xl:right-0 z-10 top-1/2 max-xl:mt-5 xl:transform xl:-translate-y-1/2 w-10 h-10 rounded-full border border-[#00171F] flex items-center justify-center hover:bg-[#00171F] swiper-arrow transition-all duration-200 ease-linear cursor-pointer">
+                                <SwiperNextArrow />
+                            </div>
                         </div>
                     </div>
                 </div>
